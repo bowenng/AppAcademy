efc4223fda62ba44bf756202e9c5cf5b0944c02c
bowenng <bowen-wu@hotmail.com>
2019-06-20 18:59:22 +0800

    Last section of SQL Tutorial

---
+++ SQL/URLShortener/db/migrate/20190620104210_create_users.rb
@@ -0,0 +1,8 @@
+class CreateUsers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :users do |t|
+      t.string :email, null: false, index: {unique: true}
+      t.timestamp
+    end
+  end
+end
